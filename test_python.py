d7a2a0cce7d21097918341f5a382361d7bab27b3
Sprau <116264571+Sprau@users.noreply.github.com>
2023-12-12 15:24:58 +0300

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -7,17 +7,23 @@ async def some_async_function():
 async def some_async_function_that_raises():
     raise ValueError("Some error message")
 
+# Использую декораторы pytest.mark.asyncio и pytest.mark.parametrize
 @pytest.mark.asyncio
 @pytest.mark.parametrize("async_function, expected_value", [
     (some_async_function, 42),
     (some_async_function_that_raises, ValueError("Some error message")),
 ])
 async def test_async_functions(async_function, expected_value):
-    if asyncio.iscoroutinefunction(async_function):
+    try:
+        # Вызываю асинхронную функцию и ждем ее выполнения
         result = await async_function()
+        # Проверяем результат выполнения
         assert result == expected_value
-    else:
-        with pytest.raises(expected_value.__class__) as exc_info:
-            await async_function()
-
-        assert str(exc_info.value) == str(expected_value)
+    except Exception as e:
+        # Если при выполнении возникла ошибка
+        if isinstance(expected_value, type(e)):
+            # Проверяем, что тип ошибки совпадает с ожидаемым
+            assert str(e) == str(expected_value)
+        else:
+            # Если тип ошибки не совпадает с ожидаемым, то выбрасываем исключение
+            raise e
